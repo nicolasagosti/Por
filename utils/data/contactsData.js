76bdeec44cf1783df17e2c2a9f13227ed9427b6c
Nicolas Agosti <agostinicolas6557@gmail.com>
2024-04-15 18:37:47 -0300

    Adress

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'agostinicolas6557@gmail.com',
     phone: '+54 9 02324 528917',
-    address: 'Argentina, Buenos Aires, Jose Andres Pacheco de Melo - 1946',
+    address: 'Argentina, Buenos Aires',
     github: 'https://github.com/nicolasagosti',
     linkedIn: 'https://www.linkedin.com/in/nicolas-agosti-b0a373225/',
     twitter: 'https://twitter.com/MechiCush'
