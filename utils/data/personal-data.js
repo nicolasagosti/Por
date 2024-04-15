849f6c3000402a6870a683f180bcba5a5482f470
Nicolas Agosti <agostinicolas6557@gmail.com>
2024-04-15 18:37:02 -0300

    Number

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is NICOLAS AGOSTI. I am a professional and enthusiastic programmer in my daily life. I learn quickly with a self-learning attitude. I love learning and exploring new technologies and I am passionate about problem solving. I love almost every aspect of web application development. My main skill is based on JavaScript and I love doing most things using JavaScript. I am available for any type of job opportunity that suits my skills and interests.",
   email: 'agostinicolas6557@gmail.com',
-  phone: '+542424528917',
+  phone: '+54 9 02324 528917',
   address: 'Argentina, Buenos Aires',
   github: 'https://github.com/nicolasagosti',
   facebook: '',
